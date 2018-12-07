e7f2895b82fb4b33bb2e6ea9d2f3696b5c8db89e
Emily Nguyen <emilymylinh@gmail.com>
2018-12-07 01:06:20 -0800

    Implement authentication and change password

---
+++ public/scripts/index.js
@@ -0,0 +1,41 @@
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB5c9mjm-mf1eViUdp-jc-PFcr8bTFgLTg",
+  authDomain: "mememaster-ej.firebaseapp.com",
+  databaseURL: "https://mememaster-ej.firebaseio.com",
+  projectId: "mememaster-ej",
+  storageBucket: "",
+  messagingSenderId: "815545865797"
+};
+firebase.initializeApp(config);
+
+function getUser() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      console.log(user.displayName + ' signed in');
+    } else {
+      // No user is signed in.
+      console.log('No user signed in');
+    }
+  });
+}
+
+function checkUser() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (!user) {
+      window.location.replace('/');
+    } 
+  });
+}
+
+function signOut() {
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    window.location.href = "/";
+  }).catch(function(error) {
+    // An error happened.
+    console.log(error);
+  });
+}
---
+++ public/scripts/password.js
@@ -0,0 +1,59 @@
+/*
+ * change password
+ */
+function changePassword() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    // Check that new passwords match
+    var newPassword = document.getElementById('new-password').value;
+    var newPasswordConfirm = document.getElementById('new-password-confirm').value;
+
+    if (newPassword !== newPasswordConfirm) {
+      alert('Passwords do not match. Try again.');
+      return;
+    }
+
+    // Call updatePassword
+    user.updatePassword(newPassword).then(function() {
+      // Update successful
+      alert('Password has been updated.');
+    }).catch(function(error) {
+      // An error happened
+      alert(error);
+
+      // If re-authentication needed, show reauth form
+      if (error.code == "auth/requires-recent-login") {
+        document.getElementById('reauth').style.display = "block";
+      }
+    });
+
+  });
+}
+
+function reauth() {
+  document.getElementById('password-form').style.display = "none";
+  firebase.auth().onAuthStateChanged(function(user) {
+    
+    // Prompt the user to re-provide their sign-in credentials
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let credential = firebase.auth.EmailAuthProvider.credential(
+        email,
+        password
+    );
+    user.reauthenticateAndRetrieveDataWithCredential(credential).then(function() {
+      // User re-authenticated.
+      console.log('Re-authenticated');
+      document.getElementById('reauth').style.display = "none";
+      document.getElementById('password-form').style.display = "block";
+      setTimeout(0, function() {
+        changePassword();
+      });
+      
+    }).catch(function(error) {
+      // An error happened.
+      alert(error);
+    });
+  });
+}
+
+  
